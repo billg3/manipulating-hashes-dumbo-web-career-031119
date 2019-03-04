e6bdfffffe8953030b21b43ff7d9acecee888edc
Bill G <gbilly3@gmail.com>
2019-03-04 16:26:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,15 +15,12 @@ def first_challenge
 
   #your code here
   
-contacts.each do |person, data|
-  
-  data.each do |attribute, value|
-    
-    if attribute == :favorite_icecream_flavors
-      
-      value. delete_if == "strawberry"
-    
+   contacts.each do |person, data|
 
+    data.each do |attribute, value|
+
+      if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor == "strawberry"}
 end 
   #remember to return your newly altered contacts hash!
   contacts
